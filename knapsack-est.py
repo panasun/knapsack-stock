fa65b30e1d472327920866b2605da8752cf46d99
shipnee <panasun@shipnee.com>
2020-06-09 22:44:31 +0700

    Revise output with index

--- knapsack-est.py
+++ knapsack-est.py
@@ -6,7 +6,7 @@ import numpy as np
 
 hash_table = []
 
-def knapsack(values, weights, W):
+def knapsack(values, indices, weights, W):
   solver = pywrapknapsack_solver.KnapsackSolver(
     pywrapknapsack_solver.KnapsackSolver.
     KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
@@ -21,24 +21,30 @@ def knapsack(values, weights, W):
   total_weight = 0
   for i in range(len(values)):
     if solver.BestSolutionContains(i):
-      packed_items.append(i)
+      packed_items.append(indices[i])
       packed_weights.append(weights[0][i])
       total_weight += weights[0][i]
-          
-  packed_weights.sort(reverse=True)
-  return (computed_value, packed_weights)
+  
+  packed_map = np.transpose(sorted(np.transpose([packed_items, packed_weights]), key=lambda x: x[0]))
+  # packed_weights.sort(reverse=True)
+  return (computed_value, packed_map[1], packed_map[0])
 
-def randomKnapsack(values, W, N):
-  np.random.shuffle(values)
+def randomKnapsack(values_input, W, N):
+  np.random.shuffle(values_input)
+
+  u = list(map(lambda x: x.split('\t'), values_input))
+  str2int = lambda x: list(map(int, x))
+  (values, indices) = (str2int(np.transpose(u)[0]), str2int(np.transpose(u)[1]))
+
   weights = [values]
-  (computed_value, packed_weights) = knapsack(values, weights, W)
-  hash_value = hash(tuple(packed_weights))
+  (computed_value, packed_weights, packed_items) = knapsack(values, indices, weights, W)
+  hash_value = hash(tuple(packed_items))
   print('%d' % (hash_value))
   if not hash_value in hash_table:
     hash_table.append(hash_value)
-    return([computed_value, len(packed_weights), hash_value, packed_weights])
+    return([computed_value, len(packed_items), hash_value, packed_weights, packed_items])
   else:
-    return([None, None, None])
+    return([None, None, None, None])
 
 def montecarlo(values, W, N, processes):
   results = []
@@ -54,14 +60,15 @@ def montecarlo(values, W, N, processes):
   for r in results:
     fo.write('%d %d %d\n' % (r[0], r[1], r[2]))
     fo.write('%s\n' % ','.join(list(map(str, r[3]))))
-    print(r[0:3])
+    fo.write('%s\n' % ','.join(list(map(str, r[4]))))
+    print(r[0:5])
 
 def main():
   fp = open('input.txt')
-  values_input = fp.read().split('\n')
-  values = list(map(int, values_input))
+  values = fp.read().split('\n')
+
   W = 301715126
-  N = 1000
+  N = 10
   processes = 16
   montecarlo(values, W, N, processes)
 
